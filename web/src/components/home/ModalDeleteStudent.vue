581d28a7a5619b89e6176ac17b6dbaaeabca39ad
emersonjesuz <josiemerson2013@gmail.com>
2024-08-19 18:05:22 -0300

    feat(web): create modal for student exclusion

---
+++ web/src/components/home/ModalDeleteStudent.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { useStudentData } from "@/piniaStore/studentData";
+import { ref } from "vue";
+interface Student {
+  name: string;
+  registrationNumber: string;
+  cpf: string;
+  email: string;
+}
+interface Props {
+  student: Student;
+}
+const dialog = ref(false);
+const props = defineProps<Props>();
+const useStudent = useStudentData();
+
+// delete student from list
+async function deleteStudent() {
+  useStudent.deleteStudent(props.student);
+  dialog.value = false;
+}
+</script>
+
+<template>
+  <div class="text-center pa-4">
+    <v-dialog v-model="dialog" max-width="500" persistent>
+      <template v-slot:activator="{ props: activatorProps }">
+        <v-btn
+          variant="text"
+          type="button"
+          id="modal-delete-student-button"
+          @click="dialog = true"
+        >
+          Excluir
+        </v-btn>
+      </template>
+
+      <v-card
+        variant="elevated"
+        prepend-icon="mdi-map-marker"
+        text="Você tem certeza que deseja excluir este aluno? Esta ação não pode ser desfeita."
+      >
+        <template v-slot:actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            id="modal-delete-student-button-cancel"
+            @click="dialog = false"
+          >
+            Cancelar
+          </v-btn>
+
+          <v-btn
+            id="modal-delete-student-button-confirm"
+            @click="deleteStudent()"
+          >
+            Confirmar
+          </v-btn>
+        </template>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<style scoped>
+#modal-delete-student-button {
+  background-color: #ffffff;
+  color: #ff203b;
+}
+#modal-delete-student-button-cancel {
+  background-color: #ffffff;
+  color: #00a0a6;
+}
+#modal-delete-student-button-confirm {
+  background-color: #ffffff;
+  color: #ff203a;
+  margin-right: 1rem;
+}
+</style>
