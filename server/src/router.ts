7df623064b4eef564bd7fbda25b3f0a8a734286c
emersonjesuz <josiemerson2013@gmail.com>
2024-08-20 23:00:49 -0300

    feat(server): add route protection with jsonwebtoken

--- server/src/router.ts
+++ server/src/router.ts
@@ -5,11 +5,15 @@ import { allStudentsController } from "./controllers/students/allStudents.contro
 import { editStudentController } from "./controllers/students/editStudent.controller";
 import { deleteStudentsController } from "./controllers/students/deleteStudents.controller";
 import { signInController } from "./controllers/users/signIn.controller";
+import { protectRouter } from "./middlewares/protectRouter.middleware";
 
 export const router = Router();
 
 // user login route
 router.post("/signIn", signInController);
+
+// route protection
+router.use(protectRouter);
 
 // student route
 router.post("/students", createStudentController);
