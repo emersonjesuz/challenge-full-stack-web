005126a413846f9eeb8dee3f6cd50b8c73a52b5e
emersonjesuz <josiemerson2013@gmail.com>
2024-08-20 21:56:58 -0300

    fix(server): change the error return to an object

--- server/src/helpers/isErroSchema.ts
+++ server/src/helpers/isErroSchema.ts
@@ -1,24 +1,16 @@
 import z from "zod";
 
-interface Error {
-  field: string;
-  message: string;
-}
-
 // checks if the error was caused by zod schema and deals with it
 export const isErroSchema = (error: any) => {
   if (error instanceof z.ZodError) {
-    const errors: Error[] = [];
-    error.issues.forEach((issue) => {
-      errors.push({
-        field: issue.path[0].toString(),
-        message: issue.message,
-      });
-    });
-    return errors;
+    return {
+      message: error.issues[0].message,
+    };
   } else {
     return {
       message: error.message,
     };
   }
 };
+
+// TODO: retorna apenas o primeiro erro da lista
