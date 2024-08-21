3d039c4f87d7224dafd43a0e62a15e075f740b73
emersonjesuz <josiemerson2013@gmail.com>
2024-08-20 21:58:56 -0300

    feat(server): add student routes

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,3 +1,14 @@
 import { Router } from "express";
+import { createStudentController } from "./controllers/students/createStudent.controller";
+import { getStudentController } from "./controllers/students/getStudent.controller";
+import { allStudentsController } from "./controllers/students/allStudents.controller";
+import { editStudentController } from "./controllers/students/editStudent.controller";
+import { deleteStudentsController } from "./controllers/students/deleteStudents.controller";
 
 export const router = Router();
+
+router.post("/students", createStudentController);
+router.get("/students/:registrationNumber", getStudentController);
+router.get("/students", allStudentsController);
+router.put("/students/:registrationNumber", editStudentController);
+router.delete("/students/:registrationNumber", deleteStudentsController);
