20c14a9ed185d28cfb9edf2620ccbc9ed2e93e1e
emersonjesuz <josiemerson2013@gmail.com>
2024-08-21 10:29:56 -0300

    feat(web): add route protection

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+
   routes: [
     {
       path: "/",
@@ -14,6 +15,14 @@ const router = createRouter({
       component: () => import("../views/Home.vue"),
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.name !== "sign-in" && !localStorage.getItem("token")) {
+    next({ name: "sign-in" });
+  } else {
+    next();
+  }
 });
 
 export default router;
